258b12a26c34481c776c2ca89db8dce81b56b139
Neiljun Cataag <neiljuncataag@gmail.com>
2016-10-15 23:31:44 +0800

    moving the main controller on the app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
 (function(){
 	'use strict';
 	var app = angular.module('ncphotoflickr', ['ui.router','ngAnimate','infinite-scroll']);
+	
+	/*start router*/
+	app.config(['$stateProvider','$locationProvider','$urlRouterProvider',
+		function(stateProvider,locationProvider,urlRouterProvider) {
+			stateProvider.state('search', {
+				url: '/search',
+				templateUrl: 'view/image-list.html',
+				controller: 'SearchController'
+			});
+			urlRouterProvider.otherwise('/');
+		}
+	]);
+	/*end router*/
+	
+	/*start main controller*/
+	app.controller('MainController', ['$scope', function(scope){
+		
+	}]);
+	/*end main controller*/
 }());
--- js/main.js
+++
@@ -1,7 +0,0 @@
-(function(){
-	'use strict';
-	var app = angular.module('ncphotoflickr');
-	app.controller('MainController', ['$scope', function(scope){
-		
-	}]);
-}());
