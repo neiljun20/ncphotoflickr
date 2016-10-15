15df6ee4ac052cd0ee70e1354c44bd46d72c1316
Neiljun Cataag <neiljuncataag@gmail.com>
2016-10-15 23:53:53 +0800

    when the seach list will open then the header will be added a class

--- js/search.js
+++ js/search.js
@@ -2,7 +2,8 @@
 	'use strict';
 	var app = angular.module('ncphotoflickr');
 	
-	app.controller('SearchController', ['$scope', function(scope){
-		
+	app.controller('SearchController', ['$scope','$animate', function(scope,animate){
+		var header = angular.element(document.querySelector('header'));
+		animate.addClass(header, 'reduce');
 	}]);
 }());
