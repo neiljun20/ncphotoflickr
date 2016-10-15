821d549750e1cc3e3773eac179a44c4961689d42
Neiljun Cataag <neiljuncataag@gmail.com>
2016-10-16 00:12:06 +0800

    adding the animation for header

--- js/search.js
+++ js/search.js
@@ -4,6 +4,6 @@
 	
 	app.controller('SearchController', ['$scope','$animate', function(scope,animate){
 		var header = angular.element(document.querySelector('header'));
-		animate.addClass(header, 'reduce');
+		animate.addClass(header, 'reduceHead');
 	}]);
 }());
