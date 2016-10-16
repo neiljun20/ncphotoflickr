e91e120e7c7df26c65c0e545c58227d7a88519bf
Neiljun Cataag <neiljuncataag@gmail.com>
2016-10-16 13:23:55 +0800

    adding a condition if the parameter is not blank it will search to avoid error

--- js/search.js
+++ js/search.js
@@ -10,33 +10,35 @@
 			this.page = 1;
 		};
 		Flickr.prototype.nextPage = function() {
-			var it = this;
-			if (it.busy) return;
-			it.busy = true;
-			http({
-				method: 'GET',
-				url: 'https://api.flickr.com/services/rest',
-				params: {
-					method: 'flickr.photos.search',
-					api_key: '39502a480a22e085c1c637732e3ba41c',
-					text: stateParams.q,
-					format: 'json',
-					nojsoncallback: 1,
-					page: it.page
-				}
-			}).success(function(response){
-				if(it.page <= response.photos.pages){
-					var result = response.photos.photo;
-					for (var i = 0; i <= result.length-1; i++) {
-						it.items.push(result[i]);
+			if(stateParams.q){
+				var it = this;
+				if (it.busy) return;
+				it.busy = true;
+				http({
+					method: 'GET',
+					url: 'https://api.flickr.com/services/rest',
+					params: {
+						method: 'flickr.photos.search',
+						api_key: '39502a480a22e085c1c637732e3ba41c',
+						text: stateParams.q,
+						format: 'json',
+						nojsoncallback: 1,
+						page: it.page
 					}
-					it.page++;
-				}
-				it.busy = false;
-			}).error(function(error){
-				console.log(error);
-				it.busy = false;
-			}.bind(it));
+				}).success(function(response){
+					if(it.page <= response.photos.pages){
+						var result = response.photos.photo;
+						for (var i = 0; i <= result.length-1; i++) {
+							it.items.push(result[i]);
+						}
+						it.page++;
+					}
+					it.busy = false;
+				}).error(function(error){
+					console.log(error);
+					it.busy = false;
+				}.bind(it));
+			}
 		};
 		return Flickr;
 	}]);
