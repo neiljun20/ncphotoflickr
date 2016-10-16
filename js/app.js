0f6020513bebbd0c4dabfd896d3b6dcde6015441
Neiljun Cataag <neiljuncataag@gmail.com>
2016-10-16 23:41:28 +0800

    done with the layout of prediction text

--- js/app.js
+++ js/app.js
@@ -20,19 +20,42 @@
 	/*end router*/
 	
 	/*start main controller*/
-	app.controller('MainController', ['$scope','$state', function(scope,state){
+	app.controller('MainController', ['$scope','$state','$compile', function(scope,state,compile){
 		scope.search = function(){
 			if(scope.search.term){
 				state.go('search',{by:scope.search.by, q: scope.search.term});	//this will go to search and add a paramter
 			}
 		}
-		/*start predection text layout*/
-		var searchBar = $('.search').offset(),
-			predictedText = $('#predictedText').css({
-				top: searchBar.top,
-				left: searchBar.left
+		
+		/*start predection text layout*/		
+		scope.showPredictedText = function(){
+			var	search = $('.search'),
+				body = $('body'),
+				searchBar = search.offset(),
+				searchBarHeight = search.height(),
+				searchBarWidth = search.width(),
+				predictedText = '<div id="predictedText"><label>asd</label></div>';
+		
+			if($('div#predictedText').length == 0){ // if not exist then insert the pridected text
+				$('body').append(compile(predictedText)(scope));
+			}
+			
+			$('#predictedText').css({
+				top: parseInt(searchBar.top + searchBarHeight + 10),
+				left: searchBar.left,
+				width: searchBarWidth
 			});
+		}
+		
+		scope.removePredictedText = function(){
+			$('#predictedText').remove();
+		}
+		
+		$(window).resize(function(){
+			scope.showPredictedText();
+		});
 		/*end predection text layout*/
+		
 	}]);
 	/*end main controller*/
 }());
