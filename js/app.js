b70ba88fe5ae5f35bbd08207c16db122d51411a5
Neiljun Cataag <neiljuncataag@gmail.com>
2016-10-17 01:21:26 +0800

    predicted text are now showing

--- js/app.js
+++ js/app.js
@@ -20,24 +20,43 @@
 	/*end router*/
 	
 	/*start main controller*/
-	app.controller('MainController', ['$scope','$state','$compile', function(scope,state,compile){
+	app.controller('MainController', ['$scope','$state','$compile','$http', function(scope,state,compile,http){
 		scope.search = function(){
 			if(scope.search.term){
 				state.go('search',{by:scope.search.by, q: scope.search.term});	//this will go to search and add a paramter
 			}
 		}
 		
-		/*start predection text layout*/		
+		/*start predection text layout*/
 		scope.showPredictedText = function(){
 			var	search = $('.search'),
 				body = $('body'),
 				searchBar = search.offset(),
 				searchBarHeight = search.height(),
 				searchBarWidth = search.width(),
-				predictedText = '<div id="predictedText"><label>asd</label></div>';
+				predictedText = '<div id="predictedText"></div>',
+				list = '',
+				method = 'flickr.photos.search',
+				apiKey = '39502a480a22e085c1c637732e3ba41c',
+				type = scope.search.by == 'all' ? '&text='+scope.search.term : '&tags='+scope.search.term+'&tag_mode=all';
 		
 			if($('div#predictedText').length == 0){ // if not exist then insert the pridected text
 				$('body').append(compile(predictedText)(scope));
+			}
+			
+			if(scope.search.term){
+				http({
+					cache: true,
+					method: 'GET',
+					url: 'https://api.flickr.com/services/rest/?method='+method+'&api_key='+apiKey+'&format=json&nojsoncallback=1'+type+'&page=1&sort=interestingness-asc'
+				}).success(function(response){
+					var result = response.photos.photo,
+						limit = response.photos.total <= 3 ? response.photos.total : 3 ;
+					for (var i = 0; i <= limit; i++) {
+						list += '<label>'+result[i].title+'</label>';
+					}
+					$('div#predictedText').html(compile(list)(scope));
+				});
 			}
 			
 			$('#predictedText').css({
